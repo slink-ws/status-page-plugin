8db184c0a7df8e7d5fd4ef97b4b95b306e104cb5
Mikhail Kantur <kami@slink.ws>
2021-02-17 20:16:06 +1000

    searcher (wip)

---
+++ src/main/java/ws/slink/statuspage/customfield/IncidentCustomFieldIndexer.java
@@ -0,0 +1,50 @@
+package ws.slink.statuspage.customfield;
+
+import com.atlassian.jira.issue.Issue;
+import com.atlassian.jira.issue.fields.CustomField;
+import com.atlassian.jira.issue.index.indexers.impl.AbstractCustomFieldIndexer;
+import com.atlassian.jira.web.FieldVisibilityManager;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.FieldType;
+import org.apache.lucene.index.DocValuesType;
+import org.apache.lucene.index.IndexOptions;
+import org.apache.lucene.index.IndexableFieldType;
+import ws.slink.statuspage.model.IssueIncident;
+
+import static com.atlassian.jira.util.dbc.Assertions.notNull;
+
+public class IncidentCustomFieldIndexer extends AbstractCustomFieldIndexer {
+
+    private final static String LINKED = "LINKED";
+
+    private final CustomField field;
+    private final FieldType luceneFieldType;
+
+    public IncidentCustomFieldIndexer(final FieldVisibilityManager fieldVisibilityManager, final CustomField customField){
+        super(fieldVisibilityManager, notNull("field", customField));
+        this.field = customField;
+        this.luceneFieldType = new FieldType();
+        this.luceneFieldType.setDocValuesType(DocValuesType.NONE);
+        this.luceneFieldType.setIndexOptions(IndexOptions.DOCS);
+        this.luceneFieldType.setStored(true);
+        this.luceneFieldType.setTokenized(false);
+    }
+
+    @Override public void addDocumentFieldsSearchable(Document document, Issue issue) {
+        addDocumentFields(document, issue, luceneFieldType);
+    }
+    @Override public void addDocumentFieldsNotSearchable(Document document, Issue issue) {
+        addDocumentFields(document, issue, luceneFieldType);
+    }
+
+    private void addDocumentFields(final Document doc, final Issue issue, final IndexableFieldType indexType) {
+        Object value = field.getValue(issue);
+        if (value != null) {
+            IssueIncident issueIncident = (IssueIncident)value;
+            doc.add(new Field(getDocumentFieldId(), issueIncident.toJsonString(), indexType));
+            doc.add(new Field(getDocumentFieldId(), LINKED, indexType));
+        }
+    }
+
+}
---
+++ src/main/java/ws/slink/statuspage/customfield/IncidentCustomFieldSearcher.java
@@ -0,0 +1,109 @@
+package ws.slink.statuspage.customfield;
+
+import com.atlassian.jira.JiraDataTypes;
+import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.issue.customfields.CustomFieldSearcher;
+import com.atlassian.jira.issue.customfields.CustomFieldValueProvider;
+import com.atlassian.jira.issue.customfields.SingleValueCustomFieldValueProvider;
+import com.atlassian.jira.issue.customfields.searchers.CustomFieldSearcherClauseHandler;
+import com.atlassian.jira.issue.customfields.searchers.SimpleCustomFieldSearcherClauseHandler;
+import com.atlassian.jira.issue.customfields.searchers.information.CustomFieldSearcherInformation;
+import com.atlassian.jira.issue.customfields.searchers.renderer.CustomFieldRenderer;
+import com.atlassian.jira.issue.customfields.searchers.transformer.CustomFieldInputHelper;
+import com.atlassian.jira.issue.customfields.searchers.transformer.FreeTextCustomFieldSearchInputTransformer;
+import com.atlassian.jira.issue.fields.CustomField;
+import com.atlassian.jira.issue.index.indexers.FieldIndexer;
+import com.atlassian.jira.issue.search.ClauseNames;
+import com.atlassian.jira.issue.search.searchers.information.SearcherInformation;
+import com.atlassian.jira.issue.search.searchers.renderer.SearchRenderer;
+import com.atlassian.jira.issue.search.searchers.transformer.SearchInputTransformer;
+import com.atlassian.jira.jql.operand.JqlOperandResolver;
+import com.atlassian.jira.jql.operator.OperatorClasses;
+import com.atlassian.jira.jql.query.ActualValueCustomFieldClauseQueryFactory;
+import com.atlassian.jira.jql.util.IndexValueConverter;
+import com.atlassian.jira.jql.util.SimpleIndexValueConverter;
+import com.atlassian.jira.jql.validator.ExactTextCustomFieldValidator;
+import com.atlassian.jira.plugin.customfield.CustomFieldSearcherModuleDescriptor;
+import com.atlassian.jira.web.FieldVisibilityManager;
+import com.atlassian.query.operator.Operator;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static com.atlassian.jira.util.dbc.Assertions.notNull;
+
+public class IncidentCustomFieldSearcher implements CustomFieldSearcher {
+
+    private final FieldVisibilityManager fieldVisibilityManager;
+    private final JqlOperandResolver jqlOperandResolver;
+    private final CustomFieldInputHelper customFieldInputHelper;
+
+    private volatile CustomFieldSearcherInformation searcherInformation;
+    private volatile SearchInputTransformer searchInputTransformer;
+    private volatile SearchRenderer searchRenderer;
+    private volatile CustomFieldSearcherClauseHandler customFieldSearcherClauseHandler;
+
+    private volatile CustomFieldSearcherModuleDescriptor moduleDescriptor;
+
+    public IncidentCustomFieldSearcher(final JqlOperandResolver jqlOperandResolver, final CustomFieldInputHelper customFieldInputHelper) {
+        this.fieldVisibilityManager = ComponentAccessor.getComponentOfType(FieldVisibilityManager.class);
+        this.jqlOperandResolver = jqlOperandResolver;
+        this.customFieldInputHelper = notNull("customFieldInputHelper", customFieldInputHelper);
+    }
+
+    public void init(CustomField customField) {
+        final ClauseNames clauseNames = customField.getClauseNames();
+        final FieldIndexer indexer = new IncidentCustomFieldIndexer(fieldVisibilityManager, customField);
+
+        final IndexValueConverter indexValueConverter = new SimpleIndexValueConverter(false);
+        final CustomFieldValueProvider customFieldValueProvider = new SingleValueCustomFieldValueProvider();
+
+        this.searcherInformation = new CustomFieldSearcherInformation(customField.getId(), customField.getNameKey(), Collections.singletonList(indexer), new AtomicReference(customField));
+        this.searchRenderer = new CustomFieldRenderer(clauseNames, getDescriptor(), customField, customFieldValueProvider, fieldVisibilityManager);
+
+        this.searchInputTransformer = new FreeTextCustomFieldSearchInputTransformer(customField, clauseNames, searcherInformation.getId(), customFieldInputHelper);  //new MultipleValuesCustomFieldSearchInputTransformer(field, names, searcherInformation.getId(), customFieldInputHelper);
+
+        this.customFieldSearcherClauseHandler = new SimpleCustomFieldSearcherClauseHandler(
+            new ExactTextCustomFieldValidator(),
+            new ActualValueCustomFieldClauseQueryFactory(customField.getId(), jqlOperandResolver, indexValueConverter, false),
+            OperatorClasses.EMPTY_ONLY_OPERATORS,
+            JiraDataTypes.TEXT
+        );
+    }
+    public void init(CustomFieldSearcherModuleDescriptor customFieldSearcherModuleDescriptor) {
+        this.moduleDescriptor = customFieldSearcherModuleDescriptor;
+    }
+
+    public SearcherInformation<CustomField> getSearchInformation() {
+        if (searcherInformation == null) {
+            throw new IllegalStateException("Attempt to retrieve SearcherInformation off uninitialised custom field searcher.");
+        }
+        return searcherInformation;
+    }
+    public SearchInputTransformer getSearchInputTransformer() {
+        if (searchInputTransformer == null) {
+            throw new IllegalStateException("Attempt to retrieve searchInputTransformer off uninitialised custom field searcher.");
+        }
+        return searchInputTransformer;
+    }
+    public SearchRenderer getSearchRenderer() {
+        if (searchRenderer == null) {
+            throw new IllegalStateException("Attempt to retrieve searchRenderer off uninitialised custom field searcher.");
+        }
+        return searchRenderer;
+    }
+    public CustomFieldSearcherModuleDescriptor getDescriptor() {
+        if (moduleDescriptor == null) {
+            throw new IllegalStateException("Attempt to retrieve moduleDescriptor off uninitialised custom field searcher.");
+        }
+        return moduleDescriptor;
+    }
+    public CustomFieldSearcherClauseHandler getCustomFieldSearcherClauseHandler() {
+        if (customFieldSearcherClauseHandler == null) {
+            throw new IllegalStateException("Attempt to retrieve customFieldSearcherClauseHandler off uninitialised custom field searcher.");
+        }
+        return customFieldSearcherClauseHandler;
+    }
+
+}
